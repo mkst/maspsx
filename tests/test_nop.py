526eb8e029a8bd4039e9edc3d1f6ada6c479b512
Mark Street <streetster@gmail.com>
2023-10-31 22:16:50 +0000

    Basic support for macros that follow $at expansion

--- tests/test_nop.py
+++ tests/test_nop.py
@@ -56,3 +56,28 @@ class TestNop(unittest.TestCase):
 
         clean_lines = strip_comments(res)
         self.assertEqual(expected_lines, clean_lines[:5])
+
+    def test_nop_at_expansion_with_macro(self):
+        """
+        Ensure we can handle a macro that follows an $at expansion
+        """
+        lines = [
+            "	lw	$19,Cameras($2)",
+            "",
+            "	.loc	2 41",
+            " #APP",
+            "	lw	$12, 0( $19 );lw	$13, 4( $19 );ctc2	$12, $0;ctc2	$13, $1;lw	$12, 8( $19 );lw	$13, 12( $19 );lw	$14, 16( $19 );ctc2	$12, $2;ctc2	$13, $3;ctc2	$14, $4",
+            " #NO_APP",
+        ]
+        expected_lines = [
+            ".set\tnoat",
+            "lui\t$at,%hi(Cameras)",
+            "addu\t$at,$at,$2",
+            "lw\t$19,%lo(Cameras)($at)",
+            ".set\tat",
+            "nop",
+        ]
+        mp = MaspsxProcessor(lines)
+        res = mp.process_lines()
+        clean_lines = strip_comments(res)
+        self.assertEqual(expected_lines, clean_lines[:6])
