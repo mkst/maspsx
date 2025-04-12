5ef10c940ca275104d3266f5b8ccf04bb97b4958
Mark Street <streetster@gmail.com>
2025-04-12 10:35:03 +0100

    Improve 'uses_at' function + add more tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -40,10 +40,6 @@ class TestMaspsxFunctions(unittest.TestCase):
         self.assertTrue(line_loads_from_reg(line, "$a0"))
         self.assertFalse(line_loads_from_reg(line, "$s1"))
 
-    def test_line_uses_at(self):
-        line = "sb   $2, g_InputSaveName($3)"
-        self.assertTrue(uses_at(line))
-
     def test_is_label(self):
         labels = [
             "$L38:",
@@ -54,3 +50,49 @@ class TestMaspsxFunctions(unittest.TestCase):
         for label in labels:
             with self.subTest():
                 self.assertTrue(is_label(label))
+
+
+class TestMaspsxUsesAt(unittest.TestCase):
+    def test_uses_at(self):
+        line = "sb   $2, g_InputSaveName($3)"
+        self.assertTrue(uses_at(line))
+
+    def test_uses_at_lo_macro(self):
+        line = "sw	$2,%lo(s_attr)($3)"
+        self.assertFalse(uses_at(line))
+
+    def test_uses_at_sw(self):
+        line = "sw	$2,D_801813A4"
+        self.assertTrue(uses_at(line))
+
+    def test_uses_at_sw_offset(self):
+        line = "sw	$3,g_CurrentRoom+40"
+        self.assertTrue(uses_at(line))
+
+    def test_uses_at_small_store(self):
+        line = "sb   $2,32767"
+        self.assertFalse(uses_at(line))
+
+    def test_uses_at_large_store(self):
+        line = "sb   $2,32768"
+        self.assertTrue(uses_at(line))
+
+    def test_uses_at_large_negative_store(self):
+        line = "sb	$2,-2147292186"
+        self.assertTrue(uses_at(line))
+
+    def test_uses_at_small_offset(self):
+        line = "sw	$2,-32768($16)"
+        self.assertFalse(uses_at(line))
+
+    def test_uses_at_large_offset(self):
+        line = "sw	$2,-32769($16)"
+        self.assertTrue(uses_at(line))
+
+    def test_uses_at_lw_small_offset(self):
+        line = "lw	$2,-32768($16)"
+        self.assertFalse(uses_at(line))
+
+    def test_uses_at_lw_large_offset(self):
+        line = "lw	$2,-32769($16)"
+        self.assertTrue(uses_at(line))
